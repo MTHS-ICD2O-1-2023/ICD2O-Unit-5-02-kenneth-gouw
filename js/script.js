3d400ff46ac9066978c264a68ef9e2d8ae4fe61a
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-04-25 18:21:17 +0000

    final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kenneth All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kenneth
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function alwaysChecked() {
+  const optionPositive = (document.getElementById("option-positive").checked)
+
+  if (optionPositive == true) {
+    const pRand = Math.floor(Math.random() * 9) + 1
+    document.getElementById("answer").innerHTML = "Your number is: " + pRand
+  } else {
+    const nRand = Math.floor((Math.random() * 9) + 1) * -1
+    document.getElementById("answer").innerHTML = "Your number is: " + nRand
+  }
+  // Output the answer to the user in the class "answer"
+}
